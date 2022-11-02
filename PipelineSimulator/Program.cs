cff323912c3164215b4b8ce4218988fd8de835ad
Bryson Brandon <78622843+brysbran@users.noreply.github.com>
2022-11-02 12:05:05 -0400

    Add files via upload

---
+++ PipelineSimulator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Pipeline_Lab{
+    public class Program{
+        static void Main(string[] args) {
+            Pipeline_Lab.Pipeline.Process();
+            //Pipeline_Lab.Pipeline.print();
+            
+        }
+    }
+}
